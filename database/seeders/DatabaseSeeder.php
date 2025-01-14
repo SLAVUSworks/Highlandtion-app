c1f9043364d8fb942914b541077dd38185c062c0
unknown <mlawq24@gmail.com>
2025-01-14 13:48:03 +0900

    LAST COMMIT BENERAN SUMPAH G BOONG

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Database\Seeders\UserSeeder;
 use Database\Seeders\ConfigSeeder;
+use Database\Seeders\ContactPageSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +19,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(UserSeeder::class);
         $this->call(ConfigSeeder::class);
+        $this->call(ContactPageSeeder::class);
         
     }
 }
